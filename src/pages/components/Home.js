f61ebc098e3350a67f92be962970c60b2d71efea
Ryan Fabek <ryanfabek@gmail.com>
2020-11-29 08:19:47 -0500

    Added project files

---
+++ src/pages/components/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react"
+import homestyle from "../../styles/home.module.css";
+
+class Home extends React.Component {
+
+    render() { 
+
+        return(
+            <div>
+                <h1>Ryan Fabek</h1>
+                <h2>Future Engineer and Innovator</h2>
+                <img src=""/>
+                <img src=""/>
+                <img src=""/>
+
+
+            </div>
+        
+        );
+
+    }
+
+}

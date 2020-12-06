f7327d07a587cf585683475eadf7f26f725aeb55
= <=>
2020-12-06 15:01:51 -0500

    [Master] Feat: Added image, title and description fields

---
+++ src/pages/components/Project.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react"
+import projectstyle from "../../styles/project.module.css";
+
+class Project extends React.Component {
+
+    render() { 
+
+        return(
+            <div className={projectstyle.container}>
+
+                <img src={this.props.image}></img>
+                <h2>{this.props.title}</h2>
+                <p>{this.props.desc}</p>
+                
+            </div>
+        
+        );
+
+    }
+
+}
+export default Project;

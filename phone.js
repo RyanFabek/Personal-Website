0f89c8c300d532642d36ea059b932c08f3573e8a
= <=>
2021-01-17 11:43:56 -0500

    Updates

---
+++ phone.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+
+    container: document.getElementById('phone_anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'phone.json'
+})
